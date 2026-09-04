eab3747fa4e904bb800389e32eaf74c7350f9635
aperionxerex-boop <260353111+aperionxerex-boop@users.noreply.github.com>
2026-09-04 23:27:47 +0530

    excede: update 2026-09-04 17:57

---
+++ P1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+};
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *newNode;
+    struct Node *temp;
+
+    newNode = malloc(sizeof(struct Node));
+    newNode ->data = 10;
+    newNode ->next = NULL;
+    head = newNode;
+
+    newNode = malloc(sizeof(struct Node));
+    newNode ->data = 20;
+    newNode ->next = NULL;
+    head ->next = newNode;
+
+    newNode = malloc(sizeof(struct Node));
+    newNode ->data = 30;
+    newNode ->next = NULL;
+    head ->next->next = newNode;
+
+    temp = head;
+    while( temp != NULL)
+    {
+        printf("%d ",temp ->data);
+        temp = temp->next;
+        
+    }
+    return 0;
+
+
+
+    
+}
