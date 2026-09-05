8ceefe536d1289b3bdc0ff721adf6ae10e5f1570
aperionxerex-boop <260353111+aperionxerex-boop@users.noreply.github.com>
2026-09-05 13:28:03 +0530

    excede: update 2026-09-05 07:58

--- P1.c
+++ P1.c
@@ -4,39 +4,48 @@ struct Node
 {
     int data;
     struct Node *next;
-    
 };
 int main()
 {
     struct Node *head = NULL;
     struct Node *newNode;
     struct Node *temp;
-
     newNode = malloc(sizeof(struct Node));
-    newNode ->data = 10;
-    newNode ->next = NULL;
+    newNode->data = 10;
+    newNode->next = NULL;
     head = newNode;
 
     newNode = malloc(sizeof(struct Node));
-    newNode ->data = 20;
-    newNode ->next = NULL;
-    head ->next = newNode;
+    newNode->data = 20;
+    newNode->next = NULL;
+    head->next = newNode;
 
     newNode = malloc(sizeof(struct Node));
-    newNode ->data = 30;
-    newNode ->next = NULL;
-    head ->next->next = newNode;
-
+    newNode->data = 30;
+    newNode->next = NULL;
+    head->next->next = newNode;
+    
+    newNode = malloc(sizeof(struct Node));
+    newNode->data = 40;
+    newNode->next = NULL;
+    
     temp = head;
-    while( temp != NULL)
+    
+    while(temp->next != NULL)
     {
-        printf("%d ",temp ->data);
+       
+       
         temp = temp->next;
-        
+
     }
+    temp->next = newNode;
     return 0;
+
+    
+}
+
+
 
 
 
     
-}
--- file.cpp
+++ file.cpp
@@ -1,1 +1,56 @@
 #include<iostream>
+using namespace std;
+class B;
+class A
+{
+    int x;
+   public : 
+    A(){
+        x = 10;
+        
+    }
+    friend void swap(A &,B &);
+    void display()
+    {
+        cout<<"A = "<<x<<endl;
+    }
+
+};
+class B
+{
+    int y;
+    public:
+    B(){
+        y = 20;
+
+    }
+    friend void swap(A &,B &);
+    void display()
+    {
+        cout<<"B = "<<y<<endl;
+
+    }
+};
+void swap(A &a,B &b)
+{
+    int temp = a.x;
+    a.x = b.y;
+    b.y = temp;
+
+}
+int main()
+{
+A obj1;
+B obj2;
+
+cout<<" BEFORE SWAP "<<endl;
+obj1.display();
+obj2.display();
+
+swap(obj1,obj2);
+cout<<" AFTER SWAP"<<endl;
+obj1.display();
+obj2.display();
+
+return 0;
+}
